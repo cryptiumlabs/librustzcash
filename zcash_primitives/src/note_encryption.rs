8fc255d9e69aa33031e220efbcee5c27d4aa933b
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2020-09-18 10:39:37 -0600

    Apply suggestions from code review
    Co-authored-by: str4d <thestr4d@gmail.com>

--- zcash_primitives/src/note_encryption.rs
+++ zcash_primitives/src/note_encryption.rs
@@ -669,11 +669,6 @@ mod tests {
             NetworkUpgrade::{Canopy, Sapling},
             Parameters, ZIP212_GRACE_PERIOD,
         },
-        //        jubjub::{
-        //            edwards,
-        //            fs::{Fs, FsRepr},
-        //            PrimeOrder, Unknown,
-        //        },
         keys::OutgoingViewingKey,
         primitives::{Diversifier, PaymentAddress, Rseed, ValueCommitment},
         util::generate_random_rseed,
